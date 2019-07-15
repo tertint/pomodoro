e8fb11cf9a9231679452450653c3177e9b20fc39
tertint <kvashnin.andrejj@gmail.com>
2019-07-15 16:56:40 +0300

    create buttons

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+let stopped = false;
+let timerId = 0;
+let _time = 60 * 25
 const startTimer = (duration, display) => {
+    if (stopped) return false;
     let timer = duration, minutes, seconds;
-    setInterval(() => {
+    timerId = setInterval(() => {
         minutes = parseInt(timer / 60, 10)
         seconds = parseInt(timer % 60, 10);
 
@@ -8,20 +12,21 @@ const startTimer = (duration, display) => {
         seconds = seconds < 10 ? "0" + seconds : seconds;
 
         display.textContent = minutes + ":" + seconds;
-
+        _time = timer;
         if (--timer < 0) {
             timer = duration;
         }
     }, 1000);
 }
 document.getElementById("start").onclick = () => {
-    let _time = 60 * 25,
-        display = document.querySelector('#timer');
+    display = document.querySelector('#timer');
     startTimer(_time, display);
 };
-
-document.getElementById("start").onclick = () => {
-    let _time = 60 * 25,
-        display = document.querySelector('#timer');
-    startTimer(_time, display);
+document.getElementById("pause").onclick = () => {
+    clearInterval(timerId);
+};
+document.getElementById("stop").onclick = () => {
+    clearInterval(timerId);
+    _time = 60 * 25;
+    document.getElementById("timer").textContent = "25:00"
 };
